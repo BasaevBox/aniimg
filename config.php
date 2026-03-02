8926a19538117c5a01b26f0dba8e7fd6d32605db
AniHou <147379824+BasaevBox@users.noreply.github.com>
2026-03-03 01:49:21 +0300

    config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'aniimg');
-define('DB_USER', 'root');
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USER', '');
 define('DB_PASS', '');
 define('UPLOAD_DIR', 'uploads/');
 ?>
